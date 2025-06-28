dbc6ec4cee7e8b4f5a0853e76b4a1ba2e0815b5d
Farid <farid.adinani@hotmail.com>
2025-06-28 20:49:55 +0200

    creation de la page QR code pour le projet foodApp

--- components/Home.js
+++ components/Home.js
@@ -4,10 +4,15 @@ function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+        <h1>FoodApp</h1>
+        <img src={"/foodapp_QR.png"} alt="" />
+        <ol>
+            <li>Installez l'application Expo sur votre smartphone</li>
+            <li>Appuyez sur "Scan QR Code</li>
+            <li>Scannez le QR Code</li>
+            <li>Testez l'application !!</li>
+        </ol>
+    </main>
     </div>
   );
 }
